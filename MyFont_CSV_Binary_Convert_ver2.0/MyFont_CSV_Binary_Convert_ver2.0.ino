79190da6eca8a56c7fe7b8d527b7f5841a30f157
mgo-tec <mgo.tec@gmail.com>
2017-05-28 23:51:13 +0900

    Weather font addition.
    Weather font addition.

---
+++ MyFont_CSV_Binary_Convert_ver2.0/MyFont_CSV_Binary_Convert_ver2.0.ino
@@ -0,0 +1,66 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+const char* MyFont_CSV_Readfile = "/font/MyFont.csv"; //読み込む CSV ファイル名
+const char* MyFont_Binary_Writefile = "/font/MyFont.fnt"; //書き込むバイナリファイル名
+
+void setup() {
+  Serial.begin(115200);
+
+  SD.begin(5, SPI, 10000000, "/sd"); //SD CS = GPIO #5
+
+  File fr = SD.open(MyFont_CSV_Readfile, FILE_READ);
+  File fw = SD.open(MyFont_Binary_Writefile, FILE_WRITE);
+  
+  uint8_t c, cH, cL, b;
+ 
+  if(fr){
+    while(1){
+      if(fr.available()){
+        c = fr.read();
+  
+        if(c == '\0'){
+          return;
+        }else if((c == '\n') || (c == '\r')){
+          Serial.println();
+        }else if(c == ','){
+          Serial.print(',');
+        }else{
+          if(c > 0x40){
+            cH = (uint8_t)((c - 0x41) + 10);
+          }else{
+            cH = (uint8_t)(c - 0x30);
+          }
+          
+          c = fr.read();
+          if( c == '\0') return;
+          
+          if(c > 0x40){
+            cL = (uint8_t)((c - 0x41) + 10);
+          }else{
+            cL = (uint8_t)(c - 0x30);
+          }
+          b = cH*16 + cL;
+          fw.write(b);
+          Serial.print(b, HEX);
+        }
+      }else{
+        return;
+      }
+      yield();
+    }
+  }else{
+    Serial.println(F(" File has not been uploaded to the flash in SD file system"));
+    delay(30000);
+  }
+  fr.close();
+  delay(10);
+  fw.close();
+  delay(10);
+
+}
+
+void loop() {
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Excel Make Font CSV 
+MyFont_ver2.0.xlsx  
+- This is an Excel file that added Kanji such as weather forecast logo and day of the week. Used to output CSV.  
+- これは天気予報ロゴや曜日等の漢字を追加したエクセルファイルです。CSV出力するために使います。  
+  
+MyFont_CSV_Binary_Convert_ver2.0.ino  
+- This is a sketch for Arduino core for the ESP32 which converts the CSV file created with MyFont_ver 2.0 to binary format.  
+- これはMyFont_ver2.0.xlsxで作ったCSVファイルをバイナリ形式に変換する、Arduino core for the ESP32 用スケッチです。  
+  
 MyFont.xlsx  
 - This is an Excel file that outputs 16 x 16 pixel font as a CSV file.   
 - これは、フォントを自作して、CSV出力する簡単なエクセルファイルです。 
